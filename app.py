482780ccdd1dd92a3654fd407c226d86a3723fc9
justin teitzel <justin.teitzel@yahoo.com>
2024-03-31 19:37:48 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from flask_swagger_ui import get_swaggerui_blueprint
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+# Define a sample resource
+class HelloWorld(Resource):
+    def get(self):
+        return jsonify({'message': 'Hello, World!'})
+
+# Add the resource to the API
+api.add_resource(HelloWorld, '/hello')
+
+
+# Configure Swagger UI
+SWAGGER_URL = '/swagger'
+API_URL = 'http://127.0.0.1:5000/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sample API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/swagger.json')
+def swagger():
+    with open('swagger.json', 'r') as f:
+        return jsonify(json.load(f))
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
